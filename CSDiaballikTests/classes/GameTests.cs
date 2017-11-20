d0b4e27e9293e6c4413ccbbd93386c6266e92ff9
Quentin Bigot <quentin.bigot@insa-rennes.fr>
2017-11-20 09:59:56 +0100

    add tests for Game class

---
+++ CSDiaballikTests/classes/GameTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSDiaballik;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSDiaballik.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void GameTest()
+        {
+            GameBoard gb;
+            AiPlayer p1;
+            AiPlayer p2;
+            Game g = new Game(gb, p1, p2);
+            Assert.AreEqual(p1, g.Player1);
+        }
+
+        [TestMethod()]
+        public void GameTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
