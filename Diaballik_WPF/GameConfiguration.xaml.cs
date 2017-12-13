89460d9995c9d75e94e437be1c32b92104f8ac87
Quentin Bigot <quentin.bigot@insa-rennes.fr>
2017-12-13 16:47:41 +0100

    Add game configuration vue

--- Diaballik_WPF/GameConfiguration.xaml.cs
+++ Diaballik_WPF/GameConfiguration.xaml.cs
@@ -23,5 +23,10 @@ namespace Diaballik_WPF
         {
             InitializeComponent();
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
