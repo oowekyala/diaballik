01056d15ed8572eabcc1c6ff515513694284280b
unknown <quentin.bigot@insa-rennes.fr>
2017-12-20 16:38:42 +0100

    Set up GameWindow

---
+++ DiaballikWPF/GameWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Diaballik.Core;
+
+namespace DiaballikWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        GameBoard gb;
+        public Window2()
+        {
+            InitializeComponent();
+            board.Rows = gb.Size;
+            board.Columns = gb.Size;
+        }
+    }
+}
